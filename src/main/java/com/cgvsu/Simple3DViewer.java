fd29c82069f56070931baa666313259e600c9c33
ivanx <ivan.grigorij@yandex.ru>
2023-01-05 00:19:48 +0300

    My firs commit in vetka!

--- src/main/java/com/cgvsu/Simple3DViewer.java
+++ src/main/java/com/cgvsu/Simple3DViewer.java
@@ -24,8 +24,6 @@ public class Simple3DViewer extends Application {
         stage.setTitle("Simple3DViewer");
         stage.setScene(scene);
         stage.show();
-        //Здесь был Никитос
-        //Here was ME!
     }
 
     public static void main(String[] args) {
